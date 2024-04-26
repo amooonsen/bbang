b7cb7d854aec479dda426e4ef61618a76e23b1af
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-04-26 20:58:20 +0900

    Accordion 적용 중, 타이핑 여부 확인 중

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,9 +1,11 @@
+import AccordionDemo from '@/components/ui/accordion/Accordion'
 import React, { useState } from 'react'
 
 
 export default function page() {
-  const [count, setCount ] = useState(0)
   return (
-    <div>asdasdasdas</div>
+    <div>
+      <AccordionDemo/>
+    </div>
   )
 }
