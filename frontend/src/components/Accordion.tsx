25f569e0fbec1e1d84f433496dce18ffd416ddbd
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-05-14 16:57:52 +0900

    [frontend] 아코디언 타입 에러 수정

--- frontend/src/components/Accordion.tsx
+++ frontend/src/components/Accordion.tsx
@@ -1,12 +1,14 @@
-"use client"
-
 import React from 'react';
 import * as Accordion from '@radix-ui/react-accordion';
-import classNames from 'classnames';
 import { ChevronDownIcon } from '@radix-ui/react-icons';
 
-const AccordionDemo = () => (
-  <Accordion.Root className="AccordionRoot" type="single" defaultValue="item-2" collapsible>
+interface AccordionTriggerProps {
+  children: React.ReactNode;
+  className?: string;
+}
+
+const AccordionComponent = () => (
+  <Accordion.Root className="AccordionRoot" type="single" defaultValue="item-1" collapsible>
     <Accordion.Item className="AccordionItem" value="item-1">
       <AccordionTrigger>Is it accessible?</AccordionTrigger>
       <AccordionContent>Yes. It adheres to the WAI-ARIA design pattern.</AccordionContent>
@@ -15,7 +17,7 @@ const AccordionDemo = () => (
     <Accordion.Item className="AccordionItem" value="item-2">
       <AccordionTrigger>Is it unstyled?</AccordionTrigger>
       <AccordionContent>
-        Yes. It's unstyled by default, giving you freedom over the look and feel.
+        {`Yes. It's unstyled by default, giving you freedom over the look and feel.`}
       </AccordionContent>
     </Accordion.Item>
 
@@ -30,27 +32,30 @@ const AccordionDemo = () => (
   </Accordion.Root>
 );
 
-const AccordionTrigger = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+const AccordionTrigger = React.forwardRef<HTMLButtonElement, AccordionTriggerProps>((props, ref) => (
   <Accordion.Header className="AccordionHeader">
     <Accordion.Trigger
-      className={classNames('AccordionTrigger', className)}
+      className={`AccordionTrigger ${props.className || ''}`}
       {...props}
-      ref={forwardedRef}
+      ref={ref}
     >
-      {children}
-      <ChevronDownIcon className="AccordionChevron" aria-hidden />
+      {props.children}
+      <ChevronDownIcon className="AccordionChevron" aria-hidden="true" />
     </Accordion.Trigger>
   </Accordion.Header>
 ));
 
-const AccordionContent = React.forwardRef(({ children, className, ...props }, forwardedRef) => (
+const AccordionContent = React.forwardRef<HTMLElement, AccordionTriggerProps>(({ children, className, ...props }) => (
   <Accordion.Content
-    className={classNames('AccordionContent', className)}
+    className={`AccordionContent ${className}`}
     {...props}
-    ref={forwardedRef}
   >
     <div className="AccordionContentText">{children}</div>
   </Accordion.Content>
 ));
 
-export default AccordionDemo;
+// 디스플레이 네임 설정 (개발 확인용)
+AccordionTrigger.displayName = 'AccordionTrigger';
+AccordionContent.displayName = 'AccordionContent';
+
+export default AccordionComponent;
