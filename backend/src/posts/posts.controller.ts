ccc6f154844aaa63715d3a5dfb582873f7bd16c5
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-05-19 16:31:29 +0900

    [backend] 컨트롤러 로직 서비스로 변경 중

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,5 +1,6 @@
-import { Controller, Get, Param } from '@nestjs/common';
+import { Body, Controller, Get, Param, Post, Put } from '@nestjs/common';
 import { PostsService } from './posts.service';
+import { PostsModule } from './posts.module';
 
 /**
  * author: string;
@@ -8,26 +9,6 @@ import { PostsService } from './posts.service';
  * likeCount: number;
  * commentCount;
  */
-
-interface Posts {
-  id: number;
-  author: string;
-  title: string;
-  content: string;
-  likeCount: number;
-  commentCount: number;
-}
-
-const posts: Posts[] = [
-  {
-    id: 1,
-    author: '조경문',
-    title: '오늘 비가 많이 오네요',
-    content: '비가 오는 날엔 ~ ~ ~ ',
-    likeCount: 100,
-    commentCount: 20
-  }
-]
 
 
 @Controller('posts')
@@ -38,22 +19,36 @@ export class PostsController {
   //  모든 POSTS 가져오기
   @Get()
   getPosts() {
-    return posts;
+    this.postsService.getAllPosts()
   }
 
   // 2 GET /posts/:id
   // url 파라미터로 매칭되는 포스트 가져오기
   @Get(':id')
-  getPost(@Param('id') id: string) {
-    return posts.find((post) => post.id === Number(id))
+  getPost(@Param('id') id: string): PostsModule {
+    return this.postsService.getPostById(+id)
   }
+
+  // 3 POST /posts
+  // POST 생성
+  @Post()
+  postPosts(
+    @Body('author') author: string,
+    @Body('title') title: string,
+    @Body('content') content: string
+  ): PostsModule {
+    return this.postsService.createPost({ author, title, content });
+  }
+
+  // 4 PUT /posts/:id
+  // id 해당되는 POST 변경
+
+  @Put(':id')
+  updatePosts() {
+
+  }
+
+
+  // 5 DELETE /posts
+  // 등록된 posts 삭제
 }
-
-// 3 POST /posts
-// POST 생성
-
-// 4 PUT /posts/:id
-// id 해당되는 POST 변경
-
-// 5 DELETE /posts
-// 등록된 posts 삭제
--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,4 +1,54 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
+
+interface PostsModel {
+  id: number;
+  author: string;
+  title: string;
+  content: string;
+  likeCount: number;
+  commentCount: number;
+}
+
+type TypePost = Pick<PostsModel, 'author' | 'title' | 'content'>
+
+let posts: PostsModel[] = [
+  {
+    id: 1,
+    author: '조경문',
+    title: '오늘 비가 많이 오네요',
+    content: '비가 오는 날엔 ~ ~ ~ ',
+    likeCount: 100,
+    commentCount: 20
+  }
+]
 
 @Injectable()
-export class PostsService {}
+export class PostsService {
+  getAllPosts(): PostsModel[] {
+    return posts;
+  }
+
+  getPostById(id: number): PostsModel {
+    const post: PostsModel = posts.find((post: PostsModel): boolean => post.id === id)
+
+    if (!post) throw new (NotFoundException)
+
+    return post
+  }
+
+  createPost({ author, title, content }: TypePost): PostsModel {
+    const newId: number = posts.length > 0 ? posts[posts.length - 1].id + 1 : 1;
+    const post: PostsModel = {
+      id: newId,
+      author,
+      title,
+      content,
+      likeCount: 0,
+      commentCount: 0
+    }
+
+    posts = [...posts, post]
+
+    return post
+  }
+}
