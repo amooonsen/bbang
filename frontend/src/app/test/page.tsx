7afaaa434518603bcb6d282d1e926618847b933a
leeyunju <sleekzoo@etribe.co.kr>
2024-05-30 16:35:21 +0900

    raidx 테스트중

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -1,16 +1,24 @@
 import AccordionDemo from '@/components/Accordion'
+import TestDemo from '@/components/TestDemo'
 import SmapleQuery from '@/components/test/SampleQuery'
-
 import RQProvider from '@/context/RQProvider'
-
+import '@radix-ui/themes/styles.css';
+import { Container, Flex, Heading, Text } from '@radix-ui/themes';
 
 export default function page() {
   return (
     <div>
-      <RQProvider>
+      <Container size="1">
+        <Flex direction="column">
+          <Heading>Coding Issue Tracker</Heading>
+          <Text color='gray'>here you can find the issue relevant to your certain project</Text>
+        </Flex>
+      </Container>
+      {/* <RQProvider>
         <AccordionDemo />
+        <TestDemo/>
         <SmapleQuery/>
-      </RQProvider>
+      </RQProvider> */}
     </div>
   )
 }
