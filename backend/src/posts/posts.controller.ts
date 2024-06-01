80f30f2738f5d0d0b4e52336443a4d007e11f4af
CHOGYEONGMOON <chonghocho72@gmail.com>
2024-06-01 17:13:05 +0900

    [backend] Repository 생성 및 typeorm 테스트 중

--- backend/src/posts/posts.controller.ts
+++ backend/src/posts/posts.controller.ts
@@ -1,30 +1,26 @@
 import { Body, Controller, Get, Param, Post, Put, Delete } from '@nestjs/common';
 import { PostsService } from './posts.service';
-import { PostsModel, TypeCreatePost, UpdatePost } from './posts.service';
+import { UpdatePost } from './posts.service';
 
 @Controller('posts')
 export class PostsController {
-  constructor(private readonly postsService: PostsService) { }
+  constructor(private readonly postsService: PostsService) {}
 
   // 1) GET /posts
   @Get()
-  getPosts(): PostsModel[] {
+  getPosts() {
     return this.postsService.getAllPosts();
   }
 
   // 2) GET /posts/:id
   @Get(':id')
-  getPost(@Param('id') id: string): PostsModel {
+  getPost(@Param('id') id: string) {
     return this.postsService.getPostById(+id);
   }
 
   // 3) POST /posts
   @Post()
-  postPosts(
-    @Body('author') author: string,
-    @Body('title') title: string,
-    @Body('content') content: string
-  ): TypeCreatePost {
+  postPosts(@Body('author') author: string, @Body('title') title: string, @Body('content') content: string) {
     return this.postsService.createPost({ author, title, content });
   }
 
@@ -34,7 +30,7 @@ export class PostsController {
     @Param('id') id: string,
     @Body('author') author: string,
     @Body('title') title: string,
-    @Body('content') content: string
+    @Body('content') content: string,
   ): UpdatePost {
     return this.postsService.updatePost({ id: +id, author, title, content });
   }
--- backend/src/posts/posts.service.ts
+++ backend/src/posts/posts.service.ts
@@ -1,6 +1,9 @@
 import { Injectable, NotFoundException } from '@nestjs/common';
+import { InjectRepository } from '@nestjs/typeorm';
+import { Repository } from 'typeorm';
+import { PostsModel } from './entities/posts.entity';
 
-export interface PostsModel {
+export interface PostModel {
   id: number;
   author: string;
   title: string;
@@ -9,50 +12,67 @@ export interface PostsModel {
   commentCount: number;
 }
 
-export type TypeCreatePost = Pick<PostsModel, 'author' | 'title' | 'content'>;
-export type UpdatePost = Omit<PostsModel, 'likeCount' | 'commentCount'>;
+export type TypeCreatePost = Pick<PostModel, 'author' | 'title' | 'content'>;
+export type UpdatePost = Omit<PostModel, 'likeCount' | 'commentCount'>;
 
-let posts: PostsModel[] = [
+let posts: PostModel[] = [
   {
     id: 1,
     author: '조경문',
     title: '오늘 비가 많이 오네요',
     content: '비가 오는 날엔 ~ ~ ~ ',
     likeCount: 100,
-    commentCount: 20
-  }
+    commentCount: 20,
+  },
 ];
 
 @Injectable()
 export class PostsService {
-  getAllPosts(): PostsModel[] {
-    return posts;
+  constructor(
+    // PostModel을 다루는 Repository를 생성한다.
+    @InjectRepository(PostsModel)
+    private readonly postRepository: Repository<PostsModel>,
+  ) {}
+
+  async getAllPosts() {
+    return await this.postRepository.find();
   }
 
-  getPostById(id: number): PostsModel {
-    const post = posts.find(post => post.id === id);
-    if (!post) {
-      throw new NotFoundException('Post not found');
-    }
+  async getPostById(id: number) {
+    const post = await this.postRepository.findOne({
+      where: {
+        id,
+      },
+    });
+
+    if (!post) throw new NotFoundException();
+
     return post;
   }
 
-  createPost({ author, title, content }: TypeCreatePost): PostsModel {
-    const newId: number = posts.length > 0 ? posts[posts.length - 1].id + 1 : 1;
-    const post: PostsModel = {
-      id: newId,
+  async createPost({ author, title, content }: TypeCreatePost) {
+    /**
+     * 1. create -> 저장할 객체를 생성한다
+     * create는 객체를 생성해주는 작업이기 때문에 동기적으로 실행됨.
+     *
+     * 2. save -> create에서 생성한 객체를 저장한다
+     */
+    const post = this.postRepository.create({
       author,
       title,
       content,
       likeCount: 0,
-      commentCount: 0
-    };
-    posts = [...posts, post];
-    return post;
+      commentCount: 0,
+    });
+
+    // 실제 db에서 가져오는 값
+    const newPost = await this.postRepository.save(post);
+
+    return newPost;
   }
 
-  updatePost({ id, author, title, content }: UpdatePost): PostsModel {
-    const post = posts.find(post => post.id === id);
+  updatePost({ id, author, title, content }: UpdatePost): PostModel {
+    const post = posts.find((post) => post.id === id);
     if (!post) {
       throw new NotFoundException('Post not found');
     }
@@ -60,19 +80,19 @@ export class PostsService {
     if (title) post.title = title;
     if (content) post.content = content;
 
-    posts = posts.map(prevPost => prevPost.id === id ? post : prevPost)
+    posts = posts.map((prevPost) => (prevPost.id === id ? post : prevPost));
 
     return post;
   }
 
   deletePost(postId: number) {
-    const post = posts.find(post => post.id === postId)
+    const post = posts.find((post) => post.id === postId);
     if (!post) {
       throw new NotFoundException('Post not found');
     }
 
-    posts = posts.filter(post => post.id !== postId)
+    posts = posts.filter((post) => post.id !== postId);
 
-    return postId
+    return postId;
   }
 }
