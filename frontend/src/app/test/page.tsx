457e663332afcc907c411b356a3da84c37491038
leeyunju <sleekzoo@etribe.co.kr>
2024-06-05 09:36:20 +0900

    test파일 merge 안해도됨

--- frontend/src/app/test/page.tsx
+++ frontend/src/app/test/page.tsx
@@ -3,7 +3,8 @@ import TestDemo from '@/components/TestDemo'
 import SmapleQuery from '@/components/test/SampleQuery'
 import RQProvider from '@/context/RQProvider'
 import '@radix-ui/themes/styles.css';
-import { Container, Flex, Heading, Text } from '@radix-ui/themes';
+import { Container, Flex, Heading, Text, Card } from '@radix-ui/themes';
+import AlertDialogDemo from '@/components/Alert'
 
 export default function page() {
   return (
@@ -12,6 +13,12 @@ export default function page() {
         <Flex direction="column">
           <Heading>Coding Issue Tracker</Heading>
           <Text color='gray'>here you can find the issue relevant to your certain project</Text>
+        </Flex>
+        <Flex gap="4" direction="column">
+          <Card>
+            <Text>hello</Text>
+            <AlertDialogDemo/>
+          </Card>
         </Flex>
       </Container>
       {/* <RQProvider>
